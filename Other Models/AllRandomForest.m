c1c2a2f25c2f5e0278663b8f12d6524698ec7d83
Vasu Arora <vasu_arora@yahoo.in>
2018-08-09 00:40:45 +0530

    Add files via upload

---
+++ Other Models/AllRandomForest.m
@@ -0,0 +1,103 @@
+clc;
+clear;
+
+XTrain = csvread('AllTrainX.csv');
+%YTrain = csvread('AllTrainY.csv');
+[~,~,YTrain] = xlsread('AllTrainY.csv');
+XTest = csvread('AllTestX.csv');
+%YTest = csvread('AllTestY.csv');
+[~,~,YTest] = xlsread('AllTestY.csv');
+
+sztrain = size(XTrain);
+ntrain = sztrain(1);
+sztest = size(XTest);
+ntest = sztest(1);
+
+bagsize = (70/100)*ntrain;
+bagsize = int16(bagsize);
+
+% training all 100 trees
+for i = 1:100
+	bagrows = [repelem(true, bagsize) repelem(false, ntrain-bagsize)];
+	bagper = randperm(ntrain);
+	bagrows = bagrows(bagper);
+	bagTrainX = XTrain(bagrows, :);
+	bagTrainY = YTrain(bagrows,:);
+	DTModel{i} = fitctree(bagTrainX, bagTrainY);
+end
+
+
+%on the training data
+
+%prediction on training data
+for i = 1:100
+	predicted2{i} = predict(DTModel{i}, XTrain);
+end
+
+%creating empty categorical array
+cat = predicted2{1,1}(1,1);
+cat(end) = [];
+
+final_pred = categorical.empty(ntrain,0);
+
+%taking mode of all predicted models
+for i = 1:ntrain
+	cat = [];
+	for j = 1:100
+		cat = [cat predicted2{1,j}(i,1)];
+    end
+    cat = categorical(cat);
+	final_pred(i,1) = mode(cat);
+end
+
+%final_pred = table(final_pred)
+%final_pred = table2array(final_pred)
+%YTrain = table2array(YTrain);
+%YTrain = cell2mat(YTrain);
+%YTrain = table2cell(YTrain);
+YTrain = categorical(YTrain);
+C = confusionmat(YTrain, final_pred);
+
+parameters = params(C(1,1), C(1,2), C(2,1), C(2,2));
+
+
+
+%prediction on test data
+for i = 1:100
+	predicted{i} = predict(DTModel{i}, XTest);
+end
+
+%creating empty categorical array
+cat = predicted{1,1}(1,1);
+cat(end) = [];
+
+final_pred = categorical.empty(ntest,0);
+
+%taking mode of all predicted models
+for i = 1:ntest
+	cat = [];
+	for j = 1:100
+		cat = [cat predicted{1,j}(i,1)];
+    end
+    cat = categorical(cat);
+	final_pred(i,1) = mode(cat);
+end
+
+%final_pred = table(final_pred)
+%final_pred = table2array(final_pred)
+%YTest = table2array(YTest);
+%YTest = cell2mat(YTest);
+YTest = categorical(YTest);
+C = confusionmat(YTest, final_pred);
+
+[sensitivity, specificity, recall, precision, fdr, accuracy] = params(C(1,1), C(1,2), C(2,1), C(2,2))
+
+
+function [sensitivity, specificity, recall, precision, fdr, accuracy] = params(tp, fp, fn, tn)
+	sensitivity = tp/(tp+fn)
+	recall = sensitivity
+	specificity = tn/(fp+tn)
+	precision = tp/(tp+fp)
+	fdr = fp/(fp+tp)
+	accuracy = (tp+tn)/(tp+tn+fp+fn)
+end
---
+++ Other Models/qda.m
@@ -0,0 +1,47 @@
+clear;
+clc;
+
+TrainX = csvread('EqualTrainX.csv');
+%YTrain = csvread('EqualTrainY.csv');
+[~,~,TrainY] = xlsread('EqualTrainY.csv');
+TestX = csvread('EqualTestX.csv');
+%YTest = csvread('EqualTestY.csv');
+[~,~,TestY] = xlsread('EqualTestY.csv');
+
+QDAMdl = fitcdiscr(TrainX,TrainY, 'DiscrimType','quadratic');
+
+Prediction = predict(QDAMdl, TestX);
+
+tp = 0;
+fp = 0;
+tn = 0;
+fn = 0;
+for i = 1:106
+    if char(Prediction{i,1}) == char(TestY{i,1})
+        if(char(Prediction{i,1}) == 'N')
+            tp = tp+1;
+        end
+        if(char(Prediction{i,1}) == 'I')
+            tn = tn+1;
+        end
+    end
+    if char(Prediction{i,1}) ~= char(TestY{i,1})
+        if(char(Prediction{i,1}) == 'N')
+            fn = fn+1;
+        end
+        if(char(Prediction{i,1}) == 'I')
+            fp = fp+1;
+        end
+    end
+end
+
+[sensitivity, specificity, recall, precision, fdr, accuracy] = params(tp, fp, fn, tn);
+
+function [sensitivity, specificity, recall, precision, fdr, accuracy] = params(tp, fp, fn, tn)
+	sensitivity = tp/(tp+fn)
+	recall = sensitivity
+	specificity = tn/(fp+tn)
+	precision = tp/(tp+fp)
+	fdr = fp/(fp+tp)
+	accuracy = (tp+tn)/(tp+tn+fp+fn)
+end
